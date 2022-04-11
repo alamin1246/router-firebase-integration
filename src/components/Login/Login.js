94b5e21d0d86bd9e45a5575cdd6272d2ad9e4e51
alamin1246 <hossainalamin1246@gmail.com>
2022-04-11 10:29:07 +0600

    added onauthstate changed to persist the auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useFirebase from '../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" name="" id="" placeholder='Enter Your Email' />
+                <br />
+                <input type="password" name="" id="" />
+                <br />
+                <input type="submit" value="Login" />
+                <br />
+            </form>
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register Now</h3>
+            <form>
+                <input type="text" placeholder='Enter Your name' />
+                <br />
+                <input type="email" name="" id="" placeholder='Enter Your Email' />
+                <br />
+                <input type="password" name="" id="" />
+                <br />
+                <input type="submit" value="Register" />
+                <br />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
